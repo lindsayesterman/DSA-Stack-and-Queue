ab6d6ea0d785324ffe65b531ab1e28d9ea721f7b
Lindsay Esterman <lindsayesterman@gmail.com>
2020-12-23 13:58:10 -0500

    queue probs

--- queue.js
+++ queue.js
@@ -27,14 +27,43 @@ class Queue {
     }
     return node.value;
   }
+  peek() {
+    if (this.first === null) {
+      this.first = null;
+    }
+    return this.first;
+  }
+  isEmpty() {
+    if (this.first === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  display() {
+    let print = "";
+    if (this.isEmpty() || this.first === null) {
+      return null;
+    }
+    let currNode = this.first;
+    if (currNode.next === null) {
+      return currNode.value;
+    }
+    while (currNode !== null) {
+      print = print + currNode.value + " -> ";
+      currNode = currNode.next;
+    }
+
+    return print;
+  }
 }
 
-function main () {
-  let starTrekQ = new Queue 
-  starTrekQ.enqueue("Kirk")
-  starTrekQ.enqueue("Spock")
-  starTrekQ.enqueue("Uhura")
-  starTrekQ.enqueue("Sulu")
-  starTrekQ.enqueue("Checkov")
-  starTrekQ.deenqueue("Spock")
+function main() {
+  let starTrekQ = new Queue();
+  starTrekQ.enqueue("Kirk");
+  starTrekQ.enqueue("Spock");
+  starTrekQ.enqueue("Uhura");
+  starTrekQ.enqueue("Sulu");
+  starTrekQ.enqueue("Checkov");
+  starTrekQ.deenqueue("Spock");
 }
